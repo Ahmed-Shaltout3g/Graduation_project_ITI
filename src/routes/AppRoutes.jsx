dd985a2b1d37416cf3eb5516851c8f0c10bb6ae1
Ahmed Shaltout <shaltouta477@gmail.com>
2025-11-13 09:28:43 +0200

    merge

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,49 +4,37 @@ import Home from "@pages/Home/Home";
 import AboutUs from "@pages/AboutUs/AboutUs";
 import Login from "@pages/Login/Login";
 import Register from "@pages/Register/Register";
-<<<<<<< HEAD
-import ProductsPage from "../pages/MarketPlace/ProductsPage";
-import ChatApp from "@pages/Chat/ChatApp";
-
-
-
-=======
-import UserLayout from "../layout/UserLayout";
 import ProductsPage from "@pages/MarketPlace/ProductsPage";
+import ChatApp from "@pages/Chat/ChatApp";
+import UserLayout from "../layout/UserLayout";
 import NotFound from "@pages/NotFound";
 import MyAds from "@pages/MyAds/MyAds";
->>>>>>> ahmed
-
 
 export default function AppRoutes() {
     return (
         <Router>
             <Routes>
+                {/* ---------------- Public Layout ---------------- */}
                 <Route element={<Layout />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/marketplace" element={<ProductsPage />} />
                     <Route path="/aboutus" element={<AboutUs />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
+                    <Route path="/chat" element={<ChatApp />} />
                     <Route path="*" element={<NotFound />} />
-                    <Route path="/marketplace" element={ <ProductsPage />} />
-                    <Route path="/chat" element={ <ChatApp />}/>
-                </Routes>
-            </Layout>
-        </BrowserRouter>
-=======
                 </Route>
 
+                {/* ---------------- User Dashboard Layout ---------------- */}
                 <Route path="/dashboard" element={<UserLayout />}>
                     <Route index element={<MyAds />} />
-                    {/* <Route path="add-ad" element={<AddAdPage />} />
-                    <Route path="profile" element={<ProfilePage />} /> */}
+                    {/* <Route path="add-ad" element={<AddAdPage />} /> */}
+                    {/* <Route path="profile" element={<ProfilePage />} /> */}
                 </Route>
 
+                {/* Catch all route */}
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
->>>>>>> ahmed
     );
 }
